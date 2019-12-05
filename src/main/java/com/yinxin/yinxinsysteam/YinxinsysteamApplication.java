8f9447be02c0268dafbba32abb957acb46f64b4b
NullPointExcption <625160751@qq.com>
2019-12-05 15:57:04 +0800

    创建项目框架

---
+++ src/main/java/com/yinxin/yinxinsysteam/YinxinsysteamApplication.java
@@ -0,0 +1,13 @@
+package com.yinxin.yinxinsysteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinxinsysteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YinxinsysteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yinxin/yinxinsysteam/YinxinsysteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinxin.yinxinsysteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinxinsysteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
